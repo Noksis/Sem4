67c61c23cb7b930b4fe6bfe77e9d90ca3cb76167
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2021-04-07 14:47:39 +0300

    Create Names.h

---
+++ myproject/Names.h
@@ -0,0 +1,19 @@
+#pragma once
+enum {
+	HORIZONT = 0,
+	VERTICAL = 1,
+	DECK4 = 4,
+	DECK3 = 3,
+	DECK2 = 2,
+	DECK1 = 1,
+	SIZE = 10,
+	HIT = 1,
+	MISS = 0,
+	KILL = 2,
+};
+
+enum ERRORS {
+	SUCCESS = 0,
+	ERR_PLACE = 1,
+
+};
