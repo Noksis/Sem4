6dda6a57dab3aef72eb926c2bec084d11b775b13
Nikita Valov <55804292+Noksis@users.noreply.github.com>
2021-04-20 09:16:40 +0300

    Update Names.h

--- myproject/Names.h
+++ myproject/Names.h
@@ -1,15 +1,22 @@
 #pragma once
-enum {
+enum Consts {
 	HORIZONT = 0,
 	VERTICAL = 1,
 	DECK4 = 4,
 	DECK3 = 3,
 	DECK2 = 2,
 	DECK1 = 1,
-	SIZE = 10,
+	SIZE = 11,
+	LSIZE = 12,
+	POISON = 2,
+	DEAD_ZONE = 3,
+	SHIP = 5,
 	HIT = 1,
+	HIT_SHIP = 5,
+	HIT_MISS = 3,
 	MISS = 0,
 	KILL = 2,
+	INJURED = 2,
 };
 
 enum ERRORS {
